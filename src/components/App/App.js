bd98cb8b63587fe8bbe8c7605e25fcbf71ec71a5
Mark Lawson <mlawson3691@gmail.com>
2017-01-20 13:55:07 -0800

    reorganize and clean up excess files

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Map from './components/Map/App.js';
+import Map from './../Map/App.js';
 
 export default class App extends Component {
   render() {
